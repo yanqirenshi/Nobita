c981d5175b2f5d517cf687e2d0ae4eeb7476b449
yanqirenshi <yanqirenshi@gmail.com>
2013-05-18 12:38:59 +0800

    update detail

--- src/cl-oso.lisp
+++ src/cl-oso.lisp
@@ -8,44 +8,50 @@
   (:use :cl :cl-who :cl-css))
 (in-package :cl-oso)
 
-(defvar *acceptor* nil)
-(defvar *dispatch-list*
-  '(("^/oso$"             'page-home)
-    ("^/oso/css/oso.css$" 'page-oso.css)))
+(defmacro dispatch (name uri body &key (content-type "test/html"))
+  `(hunchentoot:define-easy-handler (,name :uri ,uri) ()
+     (setf (hunchentoot:content-type*) ,content-type)
+     ,body))
 
-;;
-(defun dispatch-all (list)
-  (when list
-    (let ((rec (car list)))
-      (push (hunchentoot:create-regex-dispatcher (first rec) (second rec))
-	    hunchentoot:*dispatch-table*)
-      (dispatch-all (cdr list)))))
-
-;;
-(defun start-http-svr ()
-  ;; setting hunchentoot
-  (setq hunchentoot:*hunchentoot-default-external-format*
-	(flex:make-external-format :utf-8 :eol-style :lf))
-  (setq hunchentoot:*default-content-type* "text/html; charset=utf-8")
-  (setf *acceptor*
-	(hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 4242)))
-  ;; 
-  (dispatch-all *dispatch-list*))
+(defun define-page ()
+  (dispatch page-oso.css  "/oso/css/oso.css" (css-home)  :content-type "text/css")
+  (dispatch page-oso.html "/oso"             (html-home) :content-type "text/html"))
 
 
-;;
-(defun page-oso.css ()
-  (setf (hunchentoot:content-type*) "text/css")
-  (cl-css:css '((html :background \#DC8847))))
-
-
-;; home page
-(defun page-home ()
-  (setf (hunchentoot:content-type*) "text/html")
-  (cl-who:with-html-output-to-string
-      (str nil :prologue t)
+(defun html-home (&key (stream *standard-output*))
+  (cl-who:with-html-output (stream)
     (:html (:head (:title "おまえ")
 		  (:link :rel "stylesheet" :type "text/css" :href "/oso/css/oso.css"))
-	   (:body (:div "俺の仕事はおまえのもの。おまえの仕事はおまえのもの。")
-		  (:div "ログインフォーム")))))
+	   (:body (:div :id "main-image-section"
+			(:image :id  "main-image"
+				:alt "俺の仕事はおまえのもの。おまえの仕事はおまえのもの。2"))
+		  (:div :id "login-form"
+			(:lable "ID")
+			(:input :type "text" :class "inputTextPlain" :id "user-id") 
+			(:lable "パスワード")
+			(:input :type "text" :class "inputTextPlain" :id "passowrd"))))))
+
+(defun css-home ()
+    (cl-css:css '((html :background \#ffffff)
+		  (.inputTextPlain :border "solid 1px #ccc")
+
+		  (\#main-image-section 
+		   :width 555px 
+		   :height 333px 
+		   :border "solid 1px #fff"
+		   :margin-top   111px
+		   :margin-left  auto
+		   :margin-right auto
+		   )
+		  (\#main-image
+		   :width 555px 
+		   :height 333px 
+		   )
+		  
+		  (\#login-form 
+		   :width        555px
+		   :margin-left  auto
+		   :margin-right auto)
+
+		  )))
 
