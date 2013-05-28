228aee9660c5b3916fc4a7b8efd5634464a83bf1
yanqirenshi <yanqirenshi@gmail.com>
2013-05-28 08:34:29 +0800

    update detail

--- src/server.lisp
+++ src/server.lisp
@@ -7,6 +7,8 @@
       hunchentoot:*dispatch-table*)
 (push (hunchentoot:create-folder-dispatcher-and-handler "/lib/" "/var/www/lib/")
       hunchentoot:*dispatch-table*)
+(push (hunchentoot:create-folder-dispatcher-and-handler "/oso/js/" "/var/www/oso/js/")
+      hunchentoot:*dispatch-table*)
 
 (defmacro dispatch (name uri body &key (content-type "test/html"))
   `(hunchentoot:define-easy-handler (,name :uri ,uri) ()
@@ -16,16 +18,31 @@
 (defun define-page ()
   (dispatch page-oso.css        "/oso/css/oso.css" (css-home)   :content-type "text/css")
   (dispatch page-oso.html       "/oso"             (html-main)  :content-type "text/html")
-  (dispatch page-oso-login.html "/oso/login"       (html-login) :content-type "text/html"))
+  (dispatch page-oso-login.html "/oso/login"       (html-login) :content-type "text/html")
+  (dispatch omae-get.json       "/oso/rsc/omae"    (omae-get)   :content-type "application/json"))
+
+;; omae := {:omae-id xxx :name xxx :note xxx :location [x, y, z] :status xxx :thread xxx :timestamp [...] }
+;; situation := [air...]
+;; air := {:omae-id-from xxxx :omae-id-to xxx :port xxx :status xxxx :contents :timestamp [...] }
+;; TODO: omae の pool と 採番機能を作らないといけないね。
+(defun omae-get () (json:encode-json (list *omae-a*)))
 
 
+;; TODO: Window 変更時に #graph-section のサイズを調整するようにしたいね。
 (defun html-main (&key (stream *standard-output*))
   (cl-who:with-html-output (stream)
     (:html (:head (:title "俺とおまえ")
-                  (:link :rel "stylesheet" :type "text/css" :href "/oso/css/oso.css")
-                  (:script :srt "/lib/springy/springy.js")
-                  (:script :srt "/lib/springy/springyui.js"))
-           (:body (:section :id "graph-section")))))
+                  (:link :rel "stylesheet" :type "text/css" :href "/oso/css/oso.css"))
+           (:body (:section :id "graph-section"
+                            (:canvas :id "netowrk-graph-job" :width "888px" :height "388px"))
+                  ;;;
+                  ;;; Load lib
+                  ;;;
+                  (:script :src "http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js")
+                  (:script :src "http://ajax.googleapis.com/ajax/libs/jqueryui/1/jquery-ui.min.js")
+                  (:script :src "/lib/springy/springy.js")
+                  (:script :src "/lib/springy/springyui.js")
+                  (:script :src "/oso/js/home.js")))))
 
 
 (defun html-login (&key (stream *standard-output*))
@@ -45,7 +62,8 @@
 
 ;;
 (defun css-home ()
-  (cl-css:css '((html :background \#ffffff)
+  (cl-css:css '((* :padding 0px :margin 0px :font-size medium)
+                (html :background \#ffffff)
                 (.inputTextPlain :border "solid 1px #ccc")
                 ;;
                 (\#main-image-section
@@ -72,10 +90,11 @@
                 ;;;
                 ;;;
                 (\#graph-section
-                 :width  888px
-                 :height 388px
-                 :background \#ddd
+                 :padding 5px;
+                 :border "1px solid #ccc"
+                 :border-radius 3px
                  :margin auto
+                 :overflow auto
                  )
                 )))
 
