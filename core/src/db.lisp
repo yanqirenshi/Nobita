fbe02d55582b201c939f59fd0a12b94b4a4646ac
satoshi.iwasaki <satoshi.iwasaki@glpgs.com>
2018-04-27 10:44:20 +0900

    add db.lisp contents

--- core/src/db.lisp
+++ core/src/db.lisp
@@ -1,4 +1,39 @@
 (in-package :cl-user)
 (defpackage nobita.db
-  (:use #:cl))
+  (:use #:cl)
+  (:import-from :asdf
+                #:system-source-directory)
+  (:import-from :fad
+                #:list-directory)
+  (:import-from :shinra
+                #:banshou
+                #:make-banshou)
+  (:export #:*graph*
+           #:start
+           #:stop
+           #:reboot))
 (in-package :nobita.db)
+
+(defvar *graph* nil)
+
+(defvar *graph-stor-dir*
+  (merge-pathnames "../data/graph/" (system-source-directory :ter)))
+
+(defun start ()
+  (when *graph* (stop))
+  (setf *graph*
+        (make-banshou 'banshou *graph-stor-dir*)))
+
+(defun snapshot ()
+  (up:snapshot *graph*))
+
+(defun stop ()
+  (when *graph*
+    (up:stop *graph*)
+    (setf *graph* nil)))
+
+(defun reboot ()
+  (stop)
+  (mapcar #'delete-file
+          (fad:list-directory *graph-stor-dir*))
+  (start))
