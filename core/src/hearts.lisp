ef9a57e05cdf8ee7fb08cae9d7af10da2d723a49
yanqirenshi <yanqirenshi@gmail.com>
2018-05-08 16:53:07 +0900

    add karma

--- core/src/hearts.lisp
+++ core/src/hearts.lisp
@@ -1,13 +1,14 @@
 (in-package :nobit@)
 
 (defun heart-core (heart times)
+  "キューに溜った friendship を順(FILO)に処理します。"
   (declare (ignore times))
-  (when-let ((context (pop-context heart)))
-    (let ((graph (getf context :graph))
-          (friendship (getf context :friendship)))
+  (when-let ((karma (pop-karma heart)))
+    (let ((graph (getf karma :graph))
+          (friendship (getf karma :friendship)))
       (spread graph
-              (getf context :idea)
-              (getf context :source)
+              (getf karma :idea)
+              (getf karma :source)
               (get-vertex-at graph
                              (shinra::to-class friendship)
                              :%id (shinra::to-id friendship))))))
@@ -20,7 +21,8 @@
          :ceithir (make-heart "ceithir" #'heart-core)
          :coig    (make-heart "coig"    #'heart-core)
          :sia     (make-heart "sia"     #'heart-core)
-         :seachd  (make-heart "seachd"  #'heart-core))))
+         :seachd  (make-heart "seachd"  #'heart-core)))
+  "心臓はデフォルトで七つ用意する。")
 
 (defun start ()
   (dolist (heart (hash-table-values *hearts*))
