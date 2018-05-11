fdd845fd9cc508c03da269dff349f4067d5e0ad0
yanqirenshi <yanqirenshi@gmail.com>
2018-05-11 16:50:20 +0900

    move README file

--- web/js/Store.js
+++ web/js/Store.js
@@ -1,6 +1,7 @@
 class Store extends Vanilla_Redux_Store {
     constructor(reducer) {
-        super(reducer, {});
+        super(reducer, {
+        });
     }
     init () {
         this._contents = Immutable.Map({});
